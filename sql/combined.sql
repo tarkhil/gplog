83b1f748a733444a5ea5ce65ea1f534f5cc0a977
Alex Povolotsky <tarkhil@over.ru>
2023-06-07 22:29:19 +0300

    Добавил индекс, должно бы искать быстрее

--- sql/combined.sql
+++ sql/combined.sql
@@ -1,5 +1,8 @@
 -- Я не уверен в эффективности поиска по такой конструкции
 -- Впрочем, на тысяче строк ничего осмысленного сказать все равно нельзя
+-- На всякий случай, проиндексируем логи по int_id
+create index log_int_id_idx on log (int_id);
+
 create view combined as (
   select l.created, l.str, l.int_id, l.address from log l
    union
